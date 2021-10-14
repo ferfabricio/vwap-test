87c7baae898cf7e46117d930d9258bb2361752d6
Fernando Fabricio dos Santos <ferfabricio@gmail.com>
2021-10-14 02:38:18 +0000

    fix calculation and add tests

--- domain/vwap/vwap.go
+++ domain/vwap/vwap.go
@@ -5,6 +5,12 @@ import (
 	"errors"
 )
 
+const defaultLength = 200
+
+type VWAPConfig struct {
+	Length int
+}
+
 type DataPoint struct {
 	Price    float32
 	Quantity float32
@@ -17,7 +23,8 @@ type CalculationUnit struct {
 }
 
 type Vwap struct {
-	Pairs map[string]CalculationUnit
+	Pairs  map[string]CalculationUnit
+	Length int
 }
 
 func (v Vwap) AddPair(key string) {
@@ -29,23 +36,22 @@ func (v Vwap) AddPair(key string) {
 	}
 }
 
-func calculateVwapResult(tp float32, tq float32, v float32, p float32) float32 {
-	return (tp + (p * v)) / tq
+func calculateVwapResult(tp float32, tq float32) float32 {
+	return tp / tq
 }
 
-func calculateTotalsInPair(p CalculationUnit, dp DataPoint) CalculationUnit {
-	// TODO: Retrieve this length from the configuration
-	if p.DataPoints.Len() >= 200 {
+func calculateTotalsInPair(p CalculationUnit, dp DataPoint, l int) CalculationUnit {
+	if p.DataPoints.Len() >= l {
 		f := p.DataPoints.Front()
 		fv := f.Value.(DataPoint)
-		p.TotalPricePlusQuantity -= fv.Price
+		p.TotalPricePlusQuantity -= (fv.Price * fv.Quantity)
 		p.TotalQuantity -= fv.Quantity
 		p.DataPoints.Remove(f)
 	}
 	p.DataPoints.PushBack(dp)
-	p.TotalPricePlusQuantity += dp.Price * dp.Quantity
+	p.TotalPricePlusQuantity += (dp.Price * dp.Quantity)
 	p.TotalQuantity += dp.Quantity
-	p.Result = calculateVwapResult(p.TotalPricePlusQuantity, p.TotalQuantity, dp.Quantity, dp.Price)
+	p.Result = calculateVwapResult(p.TotalPricePlusQuantity, p.TotalQuantity)
 	return p
 }
 
@@ -60,6 +66,25 @@ func (v Vwap) AddTrade(key string, price float32, quantity float32) error {
 		Quantity: quantity,
 	}
 
-	v.Pairs[key] = calculateTotalsInPair(p, dp)
+	v.Pairs[key] = calculateTotalsInPair(p, dp, v.Length)
 	return nil
+}
+
+func (v Vwap) GetResult(key string) (float32, error) {
+	p, ok := v.Pairs[key]
+	if !ok {
+		return 0, errors.New("pair not present")
+	}
+
+	return p.Result, nil
+}
+
+func New() *Vwap {
+	c := VWAPConfig{
+		Length: defaultLength,
+	}
+	return &Vwap{
+		Pairs:  map[string]CalculationUnit{},
+		Length: c.Length,
+	}
 }
--- domain/vwap/vwap_test.go
+++ domain/vwap/vwap_test.go
@@ -7,9 +7,7 @@ import (
 )
 
 func TestAddPair(t *testing.T) {
-	v := Vwap{
-		Pairs: map[string]CalculationUnit{},
-	}
+	v := New()
 
 	v.AddPair(currencies.BitcoinDollar)
 	v.AddPair(currencies.EthereumBitcoin)
@@ -22,9 +20,7 @@ func TestAddPair(t *testing.T) {
 
 func TestAddTrade(t *testing.T) {
 	t.Run("Adding trade with success", func(t *testing.T) {
-		v := Vwap{
-			Pairs: map[string]CalculationUnit{},
-		}
+		v := New()
 		v.AddPair(currencies.BitcoinDollar)
 		err := v.AddTrade(currencies.BitcoinDollar, 3438.23, 0.05)
 		if err != nil {
@@ -37,12 +33,54 @@ func TestAddTrade(t *testing.T) {
 	})
 
 	t.Run("Adding trade not existent pair", func(t *testing.T) {
-		v := Vwap{
-			Pairs: map[string]CalculationUnit{},
-		}
+		v := New()
 		err := v.AddTrade(currencies.BitcoinDollar, 10.1, 1.2)
 		if err.Error() != "initilize the pair before add trades" {
 			t.Error(err)
 		}
 	})
+}
+
+func TestVWAPCalcalationResult(t *testing.T) {
+	sc := []struct {
+		length    int
+		dps       []DataPoint
+		expResult float32
+	}{
+		{
+			length: 2,
+			dps: []DataPoint{
+				{Price: 3438.23, Quantity: 0.05},
+				{Price: 3438.16, Quantity: 0.06},
+			},
+			expResult: 3438.191818,
+		},
+		{
+			length: 5,
+			dps: []DataPoint{
+				{Price: 3438.23, Quantity: 0.05},
+				{Price: 3438.16, Quantity: 0.06},
+				{Price: 3438.22, Quantity: 0.07},
+				{Price: 3438.22, Quantity: 0.08},
+				{Price: 3438.26, Quantity: 0.09},
+			},
+			expResult: 3438.221429,
+		},
+	}
+	for _, s := range sc {
+		v := Vwap{
+			Pairs:  map[string]CalculationUnit{},
+			Length: s.length,
+		}
+		v.AddPair(currencies.BitcoinDollar)
+		for _, p := range s.dps {
+			err := v.AddTrade(currencies.BitcoinDollar, p.Price, p.Quantity)
+			if err != nil {
+				t.Fail()
+			}
+		}
+		if vf, _ := v.GetResult(currencies.BitcoinDollar); vf != s.expResult {
+			t.Errorf("expected result different from calculated %f", vf)
+		}
+	}
 }
